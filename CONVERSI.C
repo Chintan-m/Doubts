3a9a02a557b34acf92a5187a06bc27c362d73ba5
Chintan-m <chintanmehta1806@gmail.com>
2017-09-05 14:53:39 +0530

    Application of STACK in Expression Evaluation
    The given C program converts the user given infix expression to postfix expression and evaluates the postfix expression

--- CONVERSI.C
+++ CONVERSI.C
@@ -1,16 +1,21 @@
+/* The given program converts the user inputed infix
+	expression to postfix expression and evaluates 
+	the postfix expression
+					*/
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
 #include<ctype.h>
 
-void push(char a);
-char pop();
+void push1(char a);
+char pop1();
+void push2(char c);
+int pop2();
 void postfix();
-
 char stack[100];
-
-int top=-1;
-
+int s[100];
+int top1=-1;
+int top2=-1;
 void main()
 {
  clrscr();
@@ -18,13 +23,12 @@ void main()
  postfix();
  getch();
 }
-
 void postfix()
 {
- int k=0,j;
+ int a,k=0,j,l;
  char i[100],p[100],temp[100];
  printf("enter the infix expression:");
- gets(temp);
+ scanf("%s",temp);
  i[0]='(';
  strcat(i,temp);
  i[strlen(i)]=')';
@@ -42,88 +46,88 @@ void postfix()
 		    {
 		     case '+' :
 
-			if(top==-1||stack[top]=='(')
+			if(top1==-1||stack[top1]=='(')
 			    {
-				push('+');
+				push1('+');
 			    }
 
 			else
 				{
-					while(top!=-1&&stack[top]!='(')
+					while(top1!=-1&&stack[top1]!='(')
 					 {
-						p[k]=pop();
+						p[k]=pop1();
 						k++;
 					 }
-					push('+');
+					push1('+');
 				}
 		     break;
 
 		     case '-':
 
-			if(top==-1||stack[top]=='(')
+			if(top1==-1||stack[top1]=='(')
 			       {
-				push('-');
+				push1('-');
 			       }
 
 			else
 				{
-					while(top!=-1&&stack[top]!='(')
+					while(top1!=-1&&stack[top1]!='(')
 					 {
-						p[k]=pop();
+						p[k]=pop1();
 						k++;
 					 }
-					push('-');
+					push1('-');
 				}
 		     break;
 
 		     case '(':
-			push('(');
+			push1('(');
 		     break;
 
 		     case')':
 
-			while(top!=-1&&stack[top]!='(')
+			while(top1!=-1&&stack[top1]!='(')
 				{
-					p[k]=pop();
+					p[k]=pop1();
 					k++;
 				}
-			 pop();
+			 pop1();
 
 		     break;
 
 		     case'^':
-			if(stack[top]!='^')
+			if(stack[top1]!='^')
 			 {
-			  push('^');
+			  push1('^');
 			 }
 
 			else
 			 {
-				while(stack[top]=='^'&&(top!=-1))
+				while(stack[top1]=='^'&&(top1!=-1))
 				 {
-					p[k]=pop();
+					p[k]=pop1();
 					k++;
 				 }
-				push('^');
+				push1('^');
 			 }
 		    break;
 
 		    case '*':
-			while((stack[top]=='*'||stack[top]=='/'||stack[top]=='^')&&(top!=-1))
+			while((stack[top1]=='*'||stack[top1]=='/'||stack[top1]=='^')&&(top1!=-1))
 			 {
-			   p[k]=pop();
+			   p[k]=pop1();
 			   k++;
 			 }
-			push('*');
+			push1('*');
 		    break;
 
 		    case '/':
-			while((stack[top]=='*'||stack[top]=='/'||stack[top]=='^')&&(top!=-1))
+			while((stack[top1]=='*'||stack[top1]=='/'||stack[top1]=='^')&&(top1!=-1))
 			{
-			 p[k]=pop();
+			 p[k]=pop1();
 			 k++;
 			}
-			push('/');
+			push1('/');
 		    break;
 
 		    default:
@@ -134,26 +138,109 @@ void postfix()
 		  }
 		}
 
-     printf("the postfix expression is:");
-     printf("%s",p);
+     printf("the postfix expression is:\n");
+     printf("%s\n",p);
+for(l=0;l<strlen(p);l++)
+{
+  if(isdigit(p[l]))
+   {
+    push2(p[l]);
+   }
+  else
+  {
+  switch(p[l])
+  {
+	int a,b,c;
+	case '+':a=pop2();
+		 b=pop2();
+		 c=b+a;
+		 push2((char)(c+48));
+		 break;
+
+	case '-':a=pop2();
+		 b=pop2();
+		 c=b-a;
+		 push2((char)(c+48));
+		 break;
+	case '*':a=pop2();
+		 b=pop2();
+		 c=b*a;
+		 push2((char)(c+48));
+		 break;
+	case '/':a=pop2();
+		 b=pop2();
+		 c=b/a;
+		 push2((char)(c+48));
+		 break;
+
+		 default:printf("invalid input\n");
+     }
+  }
 }
 
-void push(char a)
+a=pop2();
+printf("result of the given expression is:%d\n",a);
+}
+
+void push1(char a)
 {
- if(top==-1)
+ if(top1==-1)
  {
-  top=0;
-  stack[top]=a;
+  top1=0;
+  stack[top1]=a;
  }
  else
  {
-  top++;
-  stack[top]=a;
+  top1++;
+  stack[top1]=a;
  }
 }
 
-char pop()
+char pop1()
 {
-  top--;
-  return stack[top+1];
+  top1--;
+  return stack[top1+1];
 }
+
+void push2(char c)
+{
+	 int n=(int)c - 48;
+	 top2=top2++;
+	 s[top2]=n;
+}
+
+
+int pop2()
+{
+	int b;
+	 b=s[top2];
+	 top2=top2--;
+	return b;
+}
+
+/* OUTPUT
+enter the infix expression:3*(3-2)/1+4
+the postfix expression is:
+332-*1/4+
+result of the given expression is:7
+                                                                                
+                                                                                
+	  */
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
